74f9b3670945f5287a5902c28e4574863716f9c5
ahmeddavids <ahmeddavids6@gmail.com>
2026-04-21 14:19:22 +0100

    feat: update CORS middleware configuration for enhanced security

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ const bodyParser = require('body-parser');
 //middleware 
 const app = express();
 app.use(cors({
-    origin: '*'
-}));
+  credentials: true,
+  origin: ['http://localhost:3000', '*'], 
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+}))
 app.use(express.json());
 
 //swagger 
