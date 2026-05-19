5d5a055e8d7958d64e28b7de9dac913d02130670
ahmeddavids <ahmeddavids6@gmail.com>
2026-05-19 21:50:34 +0100

    Add changePassword function to allow users to update their passwords

--- controllers/users.js
+++ controllers/users.js
@@ -866,6 +866,28 @@ const getRankingsAndTopAssignments = async (req, res, next) => {
   }
 };
 
+const changePassword = async (req, res, next) => {
+  try {
+    const  userId  = req.user.id;
+    const { currentPassword, newPassword } = req.body; 
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Current password is incorrect" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    user.password = hashedPassword;
+    await user.save();
+    res.status(200).json({ message: "Password changed successfully" });
+  } catch (err) {
+    next(ApiError.badRequest(err.message));
+  }
+}
+
 module.exports ={
   getProfile,
   createUser,
@@ -892,4 +914,5 @@ module.exports ={
   getSingleStaff,
   getSingleAlumni,
   getRankingsAndTopAssignments,
+  changePassword
 }
